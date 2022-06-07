d086a45e6f8d99a76dad34d13b282892f1235e6e
JacopoNa <j.nardelli@virgilio.it>
2022-06-07 16:01:42 +0200

    milestone-2

---
+++ milestone-2/js/script.js
@@ -0,0 +1,20 @@
+for(let i = 1; i <= 100; i++) {
+
+    let divisible;
+    if (i % 3 === 0 && i % 5 === 0) {
+        divisible = 'FizzBuzz'
+    } else if (i % 3 === 0) {
+        divisible = 'Fizz'
+    } else if (i % 5 === 0) {
+        divisible = 'Buzz'
+    } else {
+        divisible = i
+    }
+
+    console.log(divisible)
+
+    const myList = document.querySelector('.list')
+    const newLi =  `<li> ${divisible} </li>`
+
+    myList.innerHTML += newLi
+}
